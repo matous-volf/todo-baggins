a75252b4d58361093bc5cfbaf949f1887f582350
Matouš Volf <git@matousvolf.cz>
2024-08-18 22:34:12 +0200

    fix: remove an unused import

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,5 +1,4 @@
 use crate::components::project_form::ProjectForm;
-use crate::server::projects::{create_project, testing};
 use dioxus::core_macro::rsx;
 use dioxus::dioxus_core::Element;
 use dioxus::prelude::*;
