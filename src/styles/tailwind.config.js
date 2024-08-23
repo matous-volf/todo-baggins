96bf50ac9beb8d150f9700b6e089a0eaf49bad51
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-08-23 18:44:15 +0200

    feat: use Inter as the default font

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     mode: "all",
     content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
     theme: {
+        fontFamily: {
+            sans: ["Inter", "sans-serif"],
+        },
         extend: {},
     },
     plugins: [],
