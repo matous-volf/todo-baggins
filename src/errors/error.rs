7827d7f7221345e0c271e0886f202e4771390f5c
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-09-08 08:21:33 +0200

    style: use `Self` in error enum `impl`s

--- src/errors/error.rs
+++ src/errors/error.rs
@@ -11,7 +11,7 @@ pub enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::ServerInternal => write!(f, "internal server error"),
+            Self::ServerInternal => write!(f, "internal server error"),
         }
     }
 }
@@ -22,7 +22,7 @@ impl FromStr for Error {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
-            "internal server error" => Error::ServerInternal,
+            "internal server error" => Self::ServerInternal,
             _ => return Err(()),
         })
     }
