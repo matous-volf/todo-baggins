94ea49b76ffd03096a03b697940cc5089ef031aa
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2024-09-10 16:15:47 +0200

    feat: internationalize the app interface

--- src/components/app.rs
+++ src/components/app.rs
@@ -4,10 +4,16 @@ use dioxus::dioxus_core::Element;
 use dioxus::prelude::*;
 use dioxus_query::prelude::{use_init_query_client};
 use crate::query::{QueryErrors, QueryKey, QueryValue};
+use dioxus_sdk::i18n::{use_init_i18n};
+use crate::internationalization::get_languages;
+use crate::server::internationalization::get_language_identifier;
 
 #[component]
 pub(crate) fn App() -> Element {
     use_init_query_client::<QueryValue, QueryErrors, QueryKey>();
+    
+    let language_identifier = use_server_future(get_language_identifier)?.unwrap().unwrap();
+    use_init_i18n(language_identifier.clone(), language_identifier, get_languages);
     
     rsx! {
         div {
