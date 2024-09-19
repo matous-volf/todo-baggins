e9817754943c0510c46ce1019044593afb9d86fa
Matouš Volf <git@matousvolf.cz>
2024-09-19 22:12:00 +0200

    refactor: use one global static collator

--- src/internationalization/mod.rs
+++ src/internationalization/mod.rs
@@ -1,11 +1,16 @@
 use std::ops::Deref;
 use std::str::FromStr;
+use std::sync::Mutex;
 use chrono::Locale;
+use dioxus::fullstack::once_cell::sync::Lazy;
 use dioxus_sdk::i18n::Language;
+use feruca::Collator;
 use unic_langid_impl::LanguageIdentifier;
 
 const EN_US: &str = include_str!("en_us.json");
 const CS_CZ: &str = include_str!("cs_cz.json");
+
+pub(crate) static COLLATOR: Lazy<Mutex<Collator>> = Lazy::new(|| Mutex::new(Collator::default()));
 
 pub(crate) fn get_languages() -> Vec<Language> {
     Vec::from([EN_US, CS_CZ]).into_iter().map(|texts| Language::from_str(texts).unwrap()).collect()
